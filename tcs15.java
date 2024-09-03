2bea6593460878481b1a6f4b45797c35e8e9b29c
prachiig <prachi-ai-ig@outlook.com>
2024-09-04 00:07:37 +0530

    Add files via upload

---
+++ tcs15.java
@@ -0,0 +1,72 @@
+package tcs_nqt;
+import java.util.*;
+
+
+// You are given a sorted array ‘arr’ containing ‘n’ integers and an integer ‘x’.
+// Implement the ‘upper bound’ function to find the index of the upper bound of 'x' in the array.
+
+// Note:
+// 1. The upper bound in a sorted array is the index of the first value that is greater than a given value. 
+// 2. If the greater value does not exist then the answer is 'n', Where 'n' is the size of the array.
+// 3. Try to write a solution that runs in log(n) time complexity.
+
+// Example:
+
+// Input : ‘arr’ = {2,4,6,7} and ‘x’ = 5,
+// Output: 2
+
+// Explanation: The upper bound of 5 is 6 in the given array, which is at index 2 (0-indexed).
+
+
+public class tcs15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();        
+        int[] arr = new int[n];
+        for (int j = 0; j < arr.length; j++) {
+            arr[j] = sc.nextInt(); 
+        }
+        sc.close();
+        int ans = upperBound(arr, x, n);
+        System.out.println("lower bound: "+ans);
+        int ansBS = upperBoundBS(arr, x, n);
+        System.out.println("lower bound BS: "+ansBS);
+    }
+
+
+    // TC - O(N)
+    // SC - O(1)
+
+    public static int upperBound(int []arr, int x, int n){
+        // Write your code here.
+        for(int i = 0; i < n; i++){
+            if(arr[i] > x)
+                return i;
+        }
+        return n;
+    }
+
+
+    // TC - O(logN)
+    // SC - O(1)
+
+    public static int upperBoundBS(int []arr, int x, int n){
+        // Write your code here.
+        int s = 0;
+        int e = n-1;
+        int mid;
+        int ans = n;
+        while(s <= e){
+            mid = s + (e - s) / 2;
+            if(arr[mid] <= x){
+                s = mid + 1;
+            }
+            else{
+                ans = mid;
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
---
+++ tcs16.java
@@ -0,0 +1,60 @@
+package tcs_nqt;
+import java.util.*;
+
+// You are given a sorted array 'arr' of distinct values and a target value 'm'. You need to search for the index of the target value in the array.
+
+// Note:
+// 1. If the value is present in the array, return its index.
+// 2. If the value is absent, determine the index where it would be inserted in the array while maintaining the sorted order. 
+// 3. The given array has distinct integers.
+// 4. The given array may be empty.
+
+// Example:
+// Input:  arr = [1, 2, 4, 7],  m = 6 
+// Output: 3
+
+// Explanation: If the given array 'arr' is: [1, 2, 4, 7] and m = 6. We insert m = 6 in the array and get 'arr' as: [1, 2, 4, 6, 7]. 
+// The position of 6 is 3 (according to 0-based indexing)
+
+public class tcs16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();        
+        int[] arr = new int[n];
+        for (int j = 0; j < arr.length; j++) {
+            arr[j] = sc.nextInt(); 
+        }
+        sc.close();
+        int ans = searchInsert(arr, x);
+        System.out.println("search index to insert: "+ans);
+    }
+
+
+    // TC - O(log N)
+    // SC - O(1)
+    public static int searchInsert(int [] arr, int m){
+        // Write your code here.
+        int s = 0;
+        int n = arr.length;
+        if(n == 0)
+            return n;
+        
+        int e = n - 1;
+        int mid;
+        int ans = n;
+        while(s <= e){
+            mid = s + (e-s)/2;
+            if(arr[mid] == m)
+                return mid;
+            if(arr[mid] < m){
+                s = mid + 1;
+            }
+            else{
+                ans = mid;
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }  
+}
