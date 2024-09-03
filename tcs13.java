f1d9f6062e9abda2980e2878c2fa6652aefe4282
prachiig <prachi-ai-ig@outlook.com>
2024-09-03 20:15:41 +0530

    Add files via upload

--- tcs13.java
+++ tcs13.java
@@ -1,5 +1,19 @@
 package tcs_nqt;
 import java.util.*;
+
+// You are given an array 'a' of 'n' integers.
+
+// A majority element in the array ‘a’ is an element that appears more than 'n' / 2 times.
+
+// Find the majority element of the array.
+
+// It is guaranteed that the array 'a' always has a majority element.
+
+// Example:
+// Input: ‘n’ = 9, ‘a’ = [2, 2, 1, 3, 1, 1, 3, 1, 1]
+// Output: 1
+
+
 public class tcs13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,16 +31,16 @@ public class tcs13 {
     public static int majorityElement(int []v) {
         // Write your code here
         int n = v.length;
-        int ans = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int  i = 0; i < n; i++){
-            int value = map.getOrDefault(v[i], 0);
-            map.put(v[i], value+1);
+            int val = map.getOrDefault(v[i], 0);
+            map.put(v[i], val+1);
         }
-        for(Map.Entry<Integer, Integer> a : map.entrySet()){
-            if(a.getValue() > n/2)
-                return a.getKey();
+        for (Map.Entry<Integer, Integer> it : map.entrySet()) {
+            if (it.getValue() > (n / 2)) {
+                return it.getKey();
+            }
         }
-        return ans;
+        return -1;
     }
 }
