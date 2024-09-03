bb59f5b8cacfeaed97b983c4bb544b453eb0c2cc
prachiig <prachi-ai-ig@outlook.com>
2024-09-03 12:08:37 +0530

    Add files via upload

--- tcs10.java
+++ tcs10.java
@@ -7,19 +7,43 @@ public class tcs10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int target = sc.nextInt();
         int[] arr = new int[n];
         for (int j = 0; j < arr.length; j++) {
             arr[j] = sc.nextInt(); 
         }
         sc.close();
-        int res = search(arr, n);
+        int res = linearsearch(arr, target);
         if(res == -1)
             System.out.println("element not found");
         else
             System.out.println("element found");
+        int ans = binarysearch(arr, target);
+        if(ans == -1)
+            System.out.println("element not found in BS");
+        else
+            System.out.println("element found in BS");
     }
 
-    public static int search(int []nums, int target) {
+    public static int binarysearch(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(mid > target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static int linearsearch(int []nums, int target) {
         // Write your code here.
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == target)
