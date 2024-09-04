33525b61228627054e1314c7dd248129525618d0
prachiig <prachi-ai-ig@outlook.com>
2024-09-05 00:17:21 +0530

    Add files via upload

--- tcs19.java
+++ tcs19.java
@@ -1,6 +1,19 @@
 package tcs_nqt;
 import java.util.*;
 
+// You are given an array 'a' of size 'n' and an integer 'k'.
+
+// Find the length of the longest subarray of 'a' whose sum is equal to 'k'.
+
+// Example :
+// Input: ‘n’ = 7 ‘k’ = 3
+// ‘a’ = [1, 2, 3, 1, 1, 1, 1]
+// Output: 3
+
+// Explanation: Subarrays whose sum = ‘3’ are:
+// [1, 2], [3], [1, 1, 1] and [1, 1, 1]
+
+
 public class tcs19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
