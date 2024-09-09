e845d808ab64442d46277eec9ed7e21b1d2e9f16
prachiig <prachi-ai-ig@outlook.com>
2024-09-09 11:42:40 +0530

    Add files via upload

--- tcs21.java
+++ tcs21.java
@@ -2,6 +2,14 @@ package tcs_nqt;
 import java.util.*;
 
 
+// Given an array 'arr' of 'n' non-negative integers, your task is to move all the zeros to the end of the array while keeping the non-zero elements at the start of the array in their original order. Return the modified array.
+
+// Example :
+// Input: ‘n’ = 5, ‘arr’ = [1, 2, 0, 0, 2, 3]
+// Output: [1, 2, 2, 3, 0, 0]
+
+// Explanation: Moved all the 0’s to the end of an array, and the rest of the elements retain the order at the start.
+
 public class tcs21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
