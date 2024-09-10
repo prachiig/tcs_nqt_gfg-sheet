0bdea503fbf821124a10130716fd9ca5c469dade
prachiig <prachi-ai-ig@outlook.com>
2024-09-10 14:50:29 +0530

    Add files via upload

--- tcs24.java
+++ tcs24.java
@@ -1,6 +1,21 @@
 package tcs_nqt;
 import java.util.*;
 
+// There is an array ‘A’ of size ‘N’ with an equal number of positive and negative elements.
+// Without altering the relative order of positive and negative numbers, you must return an array of alternative positive and negative values.
+
+// Note:
+// Start the array with a positive number. 
+// For example
+
+// Input:
+// A = [1, 2, -4, -5], N = 4
+// Output:
+// 1 -4  2 -5
+// Explanation: 
+// Positive elements = 1, 2
+// Negative elements = -4, -5
+// To maintain relative ordering, 1 must occur before 2, and -4 must occur before -5.
 
 public class tcs24 {
     public static void main(String[] args) {
