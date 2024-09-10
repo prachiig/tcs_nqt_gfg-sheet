601f78ddff25900b867c835e7b4e93b562e69bba
prachiig <prachi-ai-ig@outlook.com>
2024-09-10 14:32:03 +0530

    Add files via upload

--- tcs23.java
+++ tcs23.java
@@ -1,6 +1,19 @@
 package tcs_nqt;
 import java.util.*;
 
+
+// You are given an array/list 'prices' where the elements of the array represent the prices of the stock as they were yesterday and indices of the array represent minutes. 
+// Your task is to find and return the maximum profit you can make by buying and selling the stock. You can buy and sell the stock only once.
+
+// Note:
+
+// You can’t sell without buying first.
+// For Example:
+// For the given array [ 2, 100, 150, 120],
+// The maximum profit can be achieved by buying the stock at minute 0 when its price is Rs. 2 and selling it at minute 2 when its price is Rs. 150.
+// So, the output will be 148.
+
+
 public class tcs23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
