1fc1d12799c90e7fd6e623115e0d1e409c63a35a
prachiig <prachi-ai-ig@outlook.com>
2024-09-18 23:59:30 +0530

    Add files via upload

--- tcs33.java
+++ tcs33.java
@@ -1,6 +1,18 @@
 package tcs_nqt;
 import java.util.*;
 
+
+// Problem Statement: Given a doubly linked list, and a value ‘k’, insert a node having value ‘k’ at the end of the doubly linked list.
+// Example 1:
+
+// Input Format:
+
+// DLL: 1 <-> 2 <-> 3 <-> 4
+
+// Value to be Inserted: 6
+// Result: DLL: 1 <-> 2 <-> 3 <-> 4 <-> 6
+
+
 class Node{
     int data;
     Node next;
@@ -24,11 +36,11 @@ public class tcs33 {
         head.next.next = new Node(5);
         head.next.next.next = new Node(6);
         head.next.next.next.next = new Node(7);
+        int data = 8;
         printList(head);
-        Node newHead = insertNode(head);
+        Node newHead = insertNode(head, data);
         printList(newHead);
-        deleteNode2(head.next);
-        printList(head);
+       
         sc.close();
     }
 
@@ -49,7 +61,7 @@ public class tcs33 {
 
     public static void printList(Node head){
         while(head != null){
-            System.out.println(head.data +" ");
+            System.out.print(head.data +" ");
             head = head.next;
         }
         System.out.println();
