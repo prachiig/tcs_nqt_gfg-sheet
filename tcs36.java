02fdef7e35fc65ab2cd220c99f852fef43e03036
prachiig <prachi-ai-ig@outlook.com>
2024-09-19 20:32:24 +0530

    Add files via upload

--- tcs36.java
+++ tcs36.java
@@ -1,5 +1,17 @@
 package tcs_nqt;
 
+// Given a singly linked list of 'N' nodes. The objective is to determine the middle node of a singly linked list. 
+// However, if the list has an even number of nodes, we return the second middle node.
+// Sample Input 1 :
+// 5
+// 1 2 3 4 5
+// Sample Output 1 :
+// 3 4 5
+// Sample Input 1 :
+// 6
+// 1 2 3 4 5 6
+// Sample Output 1 :
+// 4 5 6
 
 class Node{
     int data;
