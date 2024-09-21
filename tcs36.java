008e8e6a075faae0df9c79f0505755851ff0fcfe
prachiig <prachi-ai-ig@outlook.com>
2024-09-21 11:03:18 +0530

    Add files via upload

--- tcs36.java
+++ tcs36.java
@@ -1,60 +1,42 @@
 package tcs_nqt;
+import java.util.*;
 
-// Given a singly linked list of 'N' nodes. The objective is to determine the middle node of a singly linked list. 
-// However, if the list has an even number of nodes, we return the second middle node.
-// Sample Input 1 :
-// 5
-// 1 2 3 4 5
-// Sample Output 1 :
-// 3 4 5
-// Sample Input 1 :
-// 6
-// 1 2 3 4 5 6
-// Sample Output 1 :
-// 4 5 6
-
-class Node{
-    int data;
-    Node next;
-    Node(int data1){
-        data = data1;
-        next = null;
+// Implementation of stack using array
+// TC - O(N)
+// SC - O(N)
+class stack{
+    int size = 100;
+    int[] arr = new int[size];
+    int top = -1;
+    void push(int a){
+        top++;
+        arr[top] = a;
+    }
+    int pop(){
+        int popy = arr[top];
+        top--;
+        return popy;
+    }
+    int top(){
+        return arr[top];
+    }
+    int size(){
+        return top + 1;
     }
 }
 public class tcs36 {
     public static void main(String[] args) {
-        Node head = new Node(3);
-        head.next = new Node(4);
-        head.next.next = new Node(5);
-        head.next.next.next = new Node(6);
-        head.next.next.next.next = new Node(7);
-        
-        printList(head);
-        Node newHead = findMiddle(head);
-        printList(newHead);
-    }
-    
-    public static Node findMiddle(Node head)
-    {
-        // Write your code here.
-        Node slow = head;
-        Node fast = head;
-        if(head.next == null)
-            return head;
-        while(fast != null && slow != null){
-            if(fast.next == null)
-                return slow;
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return slow;
-    }
+        stack s = new stack();
+        s.push(6);
+        s.push(3);
+        s.push(7);
+        System.out.println(s.size());
+        System.out.println(s.top());
+        System.out.println(s.pop());
+        System.out.println(s.size());
+        System.out.println(s.top());
+        System.out.println(s.pop());
 
-    public static void printList(Node head){
-        while(head != null){
-            System.out.print(head.data +" ");
-            head = head.next;
-        }
-        System.out.println();
+
     }
 }
