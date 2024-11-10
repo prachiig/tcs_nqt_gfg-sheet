a37ac23bd87d497bf00bea7b2b5d672d831009d2
prachiig <prachi-ai-ig@outlook.com>
2024-11-10 15:54:54 +0530

    Add files via upload

--- tcs49.java
+++ tcs49.java
@@ -12,7 +12,10 @@ class Node{
     }
 }
 public class tcs49 {
-    public static int minValue(Node root){
+
+    // TC - O(h) - h = height of BST  worst case complexity = O(n)
+    // SC - O(1) - no stack space
+    public static int minValue1(Node root){
         if(root == null)    return -1;
 
         Node curr = root;
@@ -22,6 +25,14 @@ public class tcs49 {
 
         return curr.data;
     }
+
+    // TC - O(h) - h = height of BST  worst case complexity = O(n)
+    // SC - 0(h) - stack space
+    public static int minValue2(Node root){
+        if(root == null || root.left == null)  return root.data;
+
+        return minValue2(root.left);
+    }
     
     public static void main(String[] args) {
         Node root = new Node(5);
@@ -29,6 +40,7 @@ public class tcs49 {
         root.left.left = new Node(1);
         root.right = new Node(7);
         root.right.right = new Node(9);
-        System.out.println("Minimum value of element is: " + minValue(root));
+        System.out.println("Minimum value of element is: " + minValue1(root));
+        System.out.println("Minimum value of element is: " + minValue2(root) + " with recursion");
     }
 }
